c2755864d950a96c09178dcb89c58c4fea9259bb
Ary <aryarte>
2023-03-30 10:56:22 +0800

    migrate to laravel

--- README.md
+++
@@ -1,4 +0,0 @@
-## GeMoo Web Development
-
-Ketik `pnpm i` untuk instal module
-Ketik `pnpm start` untuk start
--- src/routes/index.ts
+++
@@ -1,6 +0,0 @@
-import { type Request, type Response, Router } from 'express'
-export const indexRouter = Router()
-
-indexRouter.get('/', (req: Request, res: Response) => {
-  res.render('index.njk')
-})
