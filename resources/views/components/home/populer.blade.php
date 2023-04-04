d658669c8ba7cb2c71ed2e00255361f5b2df9231
ahmadhabibi14 <ahmadhabibi7159@gmail.com>
2023-04-04 18:09:50 +0800

    Migrate UI to BGeen 02

--- resources/views/components/home/populer.blade.php
+++ resources/views/components/home/populer.blade.php
@@ -1,6 +1,6 @@
 
 <div class="my-20">
-    <p class="w-36 font-semibold text-center mx-auto bg-[#40CE9B] py-1 rounded-full">Populer</p>
+    <p class="w-36 font-semibold text-center mx-auto bg-sky-500 text-slate-950 py-1 rounded-full">Populer</p>
     <div class="grid grid-cols-2 gap-20 px-40 mt-10">
         <div class="bg-[#2B3637] w-full rounded-3xl">
             <img loading="lazy" src="{{ asset('images/ml.png') }}" alt="image post" class="w-full rounded-t-3xl h-[17rem]">    
